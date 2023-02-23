71519daf8e03427f67915574ee4a3feeefb70f96
Robert Low <robert.low91@gmail.com>
2023-02-23 15:49:03 +0000

    styling

--- 01_reverse_array.rb
+++ 01_reverse_array.rb
@@ -3,16 +3,14 @@
 
 # Reverse an array of integers.
 
-class Person
-  def reverse_array(array)
-    i = 0
-    arr2 = []
-    array.length.times do # Get length of array, iterate that many times.
-      arr2 << array.reverse[i] # Reverse the array, access the element at index [i], push into new array.
-      i += 1 # Increment i, repeat loop, until length of array is reached.
-    end
-    arr2
+def reverse_array(array)
+  i = 0
+  arr2 = []
+  array.length.times do # Get length of array, iterate that many times.
+    arr2 << array.reverse[i] # Reverse the array, access the element at index [i], push into new array.
+    i += 1 # Increment i, repeat loop, until length of array is reached.
   end
+  arr2
 end
 
 p reverse_array([12, 16, 5, 9, 11, 5, 4])

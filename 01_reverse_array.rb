910027520e2f5c1be7e256da8e35436a42785aa7
Robert Low <robert.low91@gmail.com>
2023-02-23 14:20:49 +0000

    reverse array

--- 01_reverse_array.rb
+++ 01_reverse_array.rb
@@ -1,14 +1,17 @@
-def reverseArray(a)
+# Reverse Array
+# https://www.hackerrank.com/challenges/arrays-ds/problem?isFullScreen=true
+
+# Reverse an array of integers.
+
+def reverse_array(array)
   i = 0
   arr2 = []
 
-  a.length.times do
-    p arr2
-    arr2 << a.reverse[i]
-    p arr2
-    i += 1
+  array.length.times do # Get length of array, iterate that many times.
+    arr2 << array.reverse[i] # Reverse the array, access the element at index [i], push into new array.
+    i += 1 # Increment i, repeat loop, until length of array is reached.
   end
   arr2
 end
 
-reverseArray([12,16,5,9,11,5,4])
+p reverse_array([12, 16, 5, 9, 11, 5, 4])

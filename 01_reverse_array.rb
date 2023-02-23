29e3c67888b762f250bea1eb9685c2decae25ac2
Robert Low <robert.low91@gmail.com>
2023-02-23 14:05:12 +0000

    reverse array

--- 01_reverse_array.rb
+++ 01_reverse_array.rb
@@ -12,12 +12,3 @@ def reverseArray(a)
 end
 
 reverseArray([12,16,5,9,11,5,4])
-
-def rotateLeft(d, arr)
-  new_arr = arr.drop(d)
-  dropped_elements = arr[0...d]
-  new_arr << dropped_elements
-  new_arr.flatten
-end
-
-p rotateLeft(2, [1,2,3,4,5])

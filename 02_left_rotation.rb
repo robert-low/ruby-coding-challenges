71df20bb240d67788a907de61052abd641776fdd
Robert Low <robert.low91@gmail.com>
2023-02-28 10:24:09 +0000

    small refactor

--- 02_left_rotation.rb
+++ 02_left_rotation.rb
@@ -6,8 +6,7 @@
 def rotate_left(number, arr)
   new_arr = arr.drop(number) # Removes the first (number) of elements from the array and returns it.
   dropped_elements = arr[0...number] # Stores a new array of dropped elements accessed through index.
-  new_arr << dropped_elements # Push array into the new array.
-  new_arr.flatten # Flattens [new_arr, [dropped_elements]] into [new_array, dropped_elements].
+  new_arr.push(dropped_elements).flatten # Push array into the new array, flatten it -> [[]] -> []
 end
 
 p rotate_left(2, [1, 2, 3, 4, 5])

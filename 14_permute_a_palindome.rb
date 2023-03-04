2224ab947c52ad316c89ebda3629f25060cd679f
Robert Low <robert.low91@gmail.com>
2023-03-04 17:08:22 +0000

    pseudocode

---
+++ 14_permute_a_palindome.rb
@@ -0,0 +1,27 @@
+# Permute a Palindrome
+
+# Write a function that will check whether ANY permutation of the characters of the
+# input string is a palindrome. Bonus points for a solution that is efficient and/or
+# that uses only built-in language functions. Deem yourself brilliant if you can come up with a version that does not
+# use any function whatsoever.
+
+# https://www.codewars.com/kata/58ae6ae22c3aaafc58000079/train/ruby
+
+def permute_a_palindrome(input)
+  # string into array of characters save to variable
+  # shuffle the array, and reverse
+  # return false until this array is equal to original array
+
+end
+
+p permute_a_palindrome('madam') # true
+# p permute_a_palindrome('adamm') # true
+# p permute_a_palindrome('junk') # false
+# p permute_a_palindrome("a") # true
+# p permute_a_palindrome("aa") # true
+# p permute_a_palindrome("baa") # true
+# p permute_a_palindrome("aab") # true
+# p permute_a_palindrome("baabcd") # false
+# p permute_a_palindrome("racecars") # false
+# p permute_a_palindrome("abcdefghba") # false
+# p permute_a_palindrome("") # true

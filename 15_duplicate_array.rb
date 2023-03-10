6fc67fb439f29ad9295f913b81509f59e3ea80a3
Robert Low <robert.low91@gmail.com>
2023-03-10 11:33:24 +0000

    fliter map ennumerable

--- 15_duplicate_array.rb
+++ 15_duplicate_array.rb
@@ -1,5 +1,15 @@
-def contains_duplicate(nums)
-  nums.uniq != nums
+# Find all duplicates in an array
+
+# https://leetcode.com/problems/find-all-duplicates-in-an-array/
+
+# Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+
+# You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+def find_duplicates(nums)
+  nums.tally.filter_map { |num, freq| num if freq == 2 }
+  # tally to convert to hash
+  # filter out, and map to new array - nums where value (freq) is eq to 2
 end
 
-p contains_duplicate([1, 2, 3, 1]) # --> true
+p find_duplicates([4, 3, 2, 7, 8, 2, 3, 1]) # --> true

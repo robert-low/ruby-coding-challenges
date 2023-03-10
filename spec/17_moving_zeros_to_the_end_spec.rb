dced72a4fe0a7fb01eb246057a4c68846793f427
Robert Low <robert.low91@gmail.com>
2023-03-10 13:39:02 +0000

    edited test

--- spec/17_moving_zeros_to_the_end_spec.rb
+++ spec/17_moving_zeros_to_the_end_spec.rb
@@ -1,7 +1,7 @@
 require './17_moving_zeros_to_the_end.rb'
 
 RSpec.describe 'moving zeroes to the end' do
-  it 'takes an array, moving all 0 to the end, preserving other numbers' do
+  it 'takes an array, moving all 0 to the end, preserving other elements' do
     expect(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])).to eq([1, 2, 1, 1, 3, 1, 0, 0, 0, 0])
   end
 end

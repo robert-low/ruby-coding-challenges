18cb7d5ac0444f1e73f965accda4f4fb1fd023e8
Robert Low <robert.low91@gmail.com>
2023-03-15 10:05:27 +0000

    refactored

--- 23_plus_minus.rb
+++ 23_plus_minus.rb
@@ -5,18 +5,19 @@
 
 # https://www.hackerrank.com/challenges/plus-minus/problem?isFullScreen=true
 
+# store size of the array in variable --> e.g 5
+# store no. of positive nums in variable float
+# store no. of negative nums in variable float
+# store no. of zero nums in variable float
+# divide posi, neg, zero by array size variable
+
 def plus_minus(arr)
   result = []
   arr_size = arr.count.to_f
-  result.push(arr.count { |num| num > 0  }.to_f / arr_size)
-  result.push(arr.count { |num| num < 0 }.to_f / arr_size)
-  result.push(arr.count { |num| num == 0 }.to_f / arr_size)
+  result << arr.count(&:positive?).to_f / arr_size
+  result << arr.count(&:negative?).to_f / arr_size
+  result << arr.count(&:zero?).to_f / arr_size
   puts result[0], result[1], result[2]
-  # store size of the array in variable --> e.g 5
-  # store no. of positive nums in variable float
-  # store no. of negative nums in variable float
-  # store no. of zero nums in variable float
-  # divide posi, neg, zero by array size variable
 end
 
 plus_minus([1, 1, 0, -1, -1]) # --> 0.400000, 0.400000, 0.200000
